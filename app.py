d2dd5e7b25bcf38b3c6356561826fc7ac8ba5f7c
nithin-gangadharan-rangaraj <73269286+nithin-gangadharan-rangaraj@users.noreply.github.com>
2021-03-06 10:08:38 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,36 @@
+import sklearn
+import numpy as np
+import pickle
+import streamlit as st
+
+PAGE_CONFIG = {"page_title":"Diabetes Risk Prediction","page_icon":"💪","layout":"centered"}
+st.set_page_config(**PAGE_CONFIG)
+
+pickle_in = open('svm_diabetes.pkl','rb')
+model = pickle.load(pickle_in)
+
+st.title("Diabetes Risk Prediction")
+
+html = '''
+<style>
+body {
+background-image: url("https://wallpapercave.com/wp/wp2838450.jpg");
+background-size: cover;
+}
+</style>
+'''
+
+def main():
+	st.markdown(html, unsafe_allow_html=True)
+	st.subheader("*Check if you have the following symptoms*")
+  
+  age = st.slider("Select your age", 1, 150)
+  
+  gender = st.selectbox('Gender', ['Male', 'Female'])
+	if (gender == 'Male'):
+    st.write(0)
+  elif (gender == 'Female'):
+    st.write(1)
+    
+if __name__ == '__main__':
+	main()
