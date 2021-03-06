a97417bbde224be555cd6525f0a4f29cfc559199
nithin-gangadharan-rangaraj <73269286+nithin-gangadharan-rangaraj@users.noreply.github.com>
2021-03-06 10:37:38 +0530

    Update app.py

--- app.py
+++ app.py
@@ -59,8 +59,6 @@ def main():
 	check_alopecia = st.checkbox('Alopecia')
 	Alopecia = 1 if check_alopecia else 0
 	
-	pickle_in = open('/content/gdrive/MyDrive/svm_diabetes.pkl', 'rb') 
-
 	x = [Age, Gender, Polyuria, Polydipsia, sudden_weight_loss, Polyphagia, visual_blurring, Itching, Irritability, partial_paresis, Alopecia]
 	x = np.array(x)
 	print("You are at ", round(((prediction_model.predict_proba(x.reshape(1, -1))*100)[0][1]),2), "% at risk")
