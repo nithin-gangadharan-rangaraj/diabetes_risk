5a827f10c7cfa902b3d09ed17129bc1dedf266d8
nithin-gangadharan-rangaraj <73269286+nithin-gangadharan-rangaraj@users.noreply.github.com>
2021-03-22 11:18:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import streamlit as st
 PAGE_CONFIG = {"page_title":"Diabetes Risk Prediction","page_icon":"💪","layout":"centered"}
 st.set_page_config(**PAGE_CONFIG)
 
-pickle_in = open('model.pkl','rb')
+pickle_in = open('model_final_diabetes.pkl','rb')
 prediction_model = pickle.load(pickle_in)
 
 st.title("Diabetes Risk Prediction")
@@ -43,8 +43,14 @@ def main():
 	check_weight_loss = st.checkbox('Sudden Weight Loss')
 	sudden_weight_loss = 1 if check_weight_loss else 0
 	
+	check_weakness = st.checkbox('Weakness')
+	weakness = 1 if check_weakness else 0
+	
 	check_polyphagia = st.checkbox('Polyphagia')
 	Polyphagia = 1 if check_polyphagia else 0
+	
+	check_genital_thrush = st.checkbox('Genital Thrush')
+	Genital_thrush = 1 if check_genital_thrush else 0
 		
 	check_blur = st.checkbox('Visual Blurring')
 	visual_blurring = 1 if check_blur else 0
@@ -55,13 +61,16 @@ def main():
 	check_irr = st.checkbox('Irritability')
 	Irritability = 1 if check_irr else 0
 	
+	check_del = st.checkbox('Delayed healing')
+	delayed_healing = 1 if check_del else 0
+	
 	check_par = st.checkbox('Partial Paresis')
 	partial_paresis = 1 if check_par else 0
 	
 	check_alopecia = st.checkbox('Alopecia')
 	Alopecia = 1 if check_alopecia else 0
 	
-	x = [Age, Gender, Polyuria, Polydipsia, sudden_weight_loss, Polyphagia, visual_blurring, Itching, Irritability, partial_paresis, Alopecia]
+	x = [Age, Gender, Polyuria, Polydipsia, sudden_weight_loss,weakness, Polyphagia,Genital_thrush, visual_blurring, Itching, Irritability, partial_paresis,delayed_healing, Alopecia]
 	x = np.array(x)
 	result = round(((prediction_model.predict_proba(x.reshape(1, -1))*100)[0][1]),2)
 	if (st.button('Check Results')):
